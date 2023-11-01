ed928891b0cf3bd02d27c498af438eb6fc1c674b
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-11-01 10:39:52 -0600

    ran final.R on becker

---
+++ final.R
@@ -0,0 +1,95 @@
+library(tidymodels)
+library(tidyverse)
+library(vroom)
+# library(ggmosaic)
+library(glmnet)
+library(parallel)
+library(embed) 
+
+train <- vroom("train.csv")
+test <- vroom("test.csv")
+
+train$ACTION <- as.factor(train$ACTION)
+
+
+recipe.t <- recipe(ACTION ~ ., data = train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  # step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome=vars(ACTION))
+
+prepped.t <- prep(recipe.t)
+bakedSet <- bake(prepped.t, new_data = train)
+
+
+num_cores <- as.numeric(parallel::detectCores())#How many cores do I have?
+if (num_cores > 4)
+  num_cores = 6
+cl <- makePSOCKcluster(num_cores)
+doParallel::registerDoParallel(cl)
+
+
+# Make function to predict and export
+predict_export <- function(workflowName, fileName){
+  # make predictions and prep data for Kaggle format
+  
+  preds <- workflowName %>%
+    predict(new_data = test, type="prob")
+  
+  submission <- preds %>% 
+    mutate(id=row_number()) %>% 
+    rename(Action = .pred_1) %>% 
+    select(3,2)
+  
+  directory = "./submissions/"
+  path = paste0(directory,fileName)
+  
+  vroom_write(submission, file = path, delim=',')
+
+
+}
+
+
+randForestModel <- rand_forest(mtry = tune(),
+                               min_n=tune(),
+                               trees=500) %>% 
+  set_engine("ranger") %>% 
+  set_mode("classification")
+
+forestWF <- workflow() %>% 
+  # add_recipe(recipe.b) %>%
+  add_recipe(recipe.t) %>% 
+  add_model(randForestModel)
+
+# create tuning grid
+forest_tuning_grid <- grid_regular(mtry(range = c(1,(ncol(bakedSet)-1))),
+                                   min_n(),
+                                   levels = 10)
+
+# split data for cross validation
+rfolds <- vfold_cv(train, v = 5, repeats=1)
+
+cvStart <- proc.time()
+
+# run cross validation
+treeCVResults <- forestWF %>% 
+  tune_grid(resamples = rfolds,
+            grid = forest_tuning_grid,
+            metrics=metric_set(roc_auc)) 
+
+print("CV time:")
+proc.time()-cvStart
+
+# select best model
+best_tuneForest <- treeCVResults %>% 
+  select_best("roc_auc")
+
+# finalize workflow
+finalForestWF <- 
+  forestWF %>% 
+  finalize_workflow(best_tuneForest) %>% 
+  fit(data=train)
+
+# predict and export
+predict_export(finalForestWF,"ClassificationForest3.csv")
+
+stopCluster(cl)
